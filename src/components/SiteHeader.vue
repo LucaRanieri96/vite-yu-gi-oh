3b4e45adf33f674c04552f8574a1691b246c9a27
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-04-17 14:28:18 +0200

    Add header content

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -5,10 +5,19 @@ export default {
 </script>
 
 <template>
- 
+  <header class="d-flex align-items-center gap-4">
+    <img src="../assets/imgs/Yugioh_anime_logo.webp" alt="" />
+    <h1>Yu-Gi-Oh Api</h1>
+  </header>
 </template>
 
 <style scoped lang="scss">
 @use "../styles/partials/_variables.scss" as *;
 
+header{
+  height: 100px;
+}
+img{
+  max-height: 50%;
+}
 </style>
